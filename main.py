5f36366a7e6294ba48dacf35f44375daf83e654b
Will Patton <willpat15@gmail.com>
2019-08-10 15:41:13 -0500

    Got part of the vhs mod done

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import datetime
 from PIL import Image
 from constants import url
 import json
+import random
+
+def decision(probability):
+    return random.random() < probability
 
 def getImage():
     now = datetime.datetime.now()
@@ -16,15 +20,39 @@ def getImage():
     with open("uhd.jpg", "wb") as f:
         f.write(r.content)
 
-def mod_image(imgname):
+def mod_image(imgname, chance_of_row_repeat=0, max_row_repeats=0, min_row_repeats=0):
     img = Image.open(imgname)
     pixels = img.load()
     width, height = img.size
+
+    repeat = False
+    num_repeats = 0
+    times_to_repeat = 0
+    row_to_repeat = []
     for y in range(height):
+        if not repeat and decision(chance_of_row_repeat):
+            repeat = True
+            times_to_repeat = random.randint(min_row_repeats, max_row_repeats)
+            print("Repeat true: " + str(times_to_repeat))
         for x in range(width):
             r, g, b = img.getpixel((x, y))
-            pixels[x, y] = (b, b, b)
+
+            if repeat and len(row_to_repeat) != width:
+                pixels[x, y] = (r, g, b)
+                row_to_repeat.append((r, g, b))
+            elif repeat:
+                pixels[x, y] = row_to_repeat[x]
+            else:
+                pixels[x, y] = (r, g, b)
+        
+        if repeat:
+            num_repeats += 1
+            if num_repeats >= times_to_repeat:
+                repeat = False
+                times_to_repeat = 0
+                num_repeats = 0
+                row_to_repeat = []
     img.save("test1.jpg")
 
 getImage()
-mod_image("uhd.jpg")
+mod_image("uhd.jpg", 0.008, 30, 10)
