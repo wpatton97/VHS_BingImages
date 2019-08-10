14a1684366542823fb0868274381080f7b71d3f2
Will Patton <willpat15@gmail.com>
2019-08-10 18:40:47 -0500

    Sine waves offsets

--- main.py
+++ main.py
@@ -7,7 +7,17 @@ import json
 import numpy as np
 import imageio
 import random
+from math import floor
 
+
+def generate_offsets(array_size, max_offset):
+    periodicity = random.randint(1, 20)
+    periodicity = random.random() * periodicity
+    offsets = []
+    for i in range(array_size):
+        # print(floor(max_offset*np.sin(periodicity*(i*np.pi/180))))
+        offsets.append(floor(max_offset*np.sin(periodicity*(i*np.pi/180))))
+    return offsets
 
 
 def HSVColor(img):
@@ -52,10 +62,12 @@ def mod_image_repeat_rows(imgname, chance_of_row_repeat=0, max_row_repeats=0, mi
     num_repeats = 0
     times_to_repeat = 0
     row_to_repeat = []
+    offsets = []
     for y in range(height):
         if not repeat and decision(chance_of_row_repeat):
             repeat = True
             times_to_repeat = random.randint(min_row_repeats, max_row_repeats)
+            offsets = generate_offsets(times_to_repeat, random.randint(10, 50))
             print("Repeat true: " + str(times_to_repeat))
         for x in range(width):
             r, g, b = img.getpixel((x, y))
@@ -64,7 +76,10 @@ def mod_image_repeat_rows(imgname, chance_of_row_repeat=0, max_row_repeats=0, mi
                 pixels[x, y] = (r, g, b)
                 row_to_repeat.append((r, g, b))
             elif repeat:
-                pixels[x, y] = row_to_repeat[x]
+                try:
+                    pixels[x, y] = row_to_repeat[x + offsets[num_repeats]]
+                except Exception as e:
+                    pixels[x, y] = row_to_repeat[x - offsets[num_repeats]]
             else:
                 pixels[x, y] = (r, g, b)
         
@@ -75,6 +90,7 @@ def mod_image_repeat_rows(imgname, chance_of_row_repeat=0, max_row_repeats=0, mi
                 times_to_repeat = 0
                 num_repeats = 0
                 row_to_repeat = []
+                offsets = []
     if save:
         img.save("test1.jpg")
 
@@ -94,5 +110,7 @@ def offset_hue(image):
 #getImage()
 mod_image_repeat_rows("uhd.jpg", 0.012, 50, 10)
 add_img_noise("test1.jpg")
+
+#generate_offsets(30, 50)
 
 #offset_hue("uhd.jpg")
