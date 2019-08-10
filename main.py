3b6075852aa9cff695687dd5060b60b7386701dc
Will Patton <willpat15@gmail.com>
2019-08-10 17:44:45 -0500

    Got noise added correctly

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 import requests
 import datetime
 from PIL import Image
+import colorsys
 from constants import url
 import json
+import numpy as np
+import imageio
 import random
+
+
+
+def HSVColor(img):
+    if isinstance(img,Image.Image):
+        r,g,b = img.split()
+        Hdat = []
+        Sdat = []
+        Vdat = [] 
+        for rd,gn,bl in zip(r.getdata(),g.getdata(),b.getdata()) :
+            h,s,v = colorsys.rgb_to_hsv(rd/255.,gn/255.,bl/255.)
+            Hdat.append(int(h*255.))
+            Sdat.append(int(s*255.))
+            Vdat.append(int(v*255.))
+        r.putdata(Hdat)
+        g.putdata(Sdat)
+        b.putdata(Vdat)
+        return Image.merge('RGB',(r,g,b))
+    else:
+        return None
 
 def decision(probability):
     return random.random() < probability
@@ -55,5 +78,21 @@ def mod_image_repeat_rows(imgname, chance_of_row_repeat=0, max_row_repeats=0, mi
     if save:
         img.save("test1.jpg")
 
-getImage()
-mod_image_repeat_rows("uhd.jpg", 0.008, 30, 10)
+
+def add_img_noise(imgpath):
+    img = imageio.imread(imgpath, pilmode='RGB')
+    noise1 = img + 1 * img.std() * np.random.random(img.shape)
+    imageio.imwrite("test2.jpg", noise1)
+
+def offset_hue(image):
+    if isinstance(image, str):
+        image = Image.open(image)
+        image = HSVColor(image)
+        print(image.mode)
+    pass
+    
+#getImage()
+mod_image_repeat_rows("uhd.jpg", 0.012, 50, 10)
+add_img_noise("test1.jpg")
+
+#offset_hue("uhd.jpg")
